dd9d1fc647370948a467bd76a24621247fe58b44
Shubham Trivedi <strivedi_be19@thapar.edu>
2020-09-01 13:29:51 +0530

    adding dynamic pieces

--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import BurgerSection from './BurgerSection/BurgerSection';
 import ManageSection from './ManageSection/ManageSection';
 import Aux from '../HOC/Aux/Aux'
 import Header from '../Header/Header';
-const BurgerBuilder=( props )=>{
-  return(
-    <Aux>
-      <Header/>
-      <BurgerSection />
-      <ManageSection />
-    </Aux>
-  )
+class BurgerBuilder extends Component{
+  state={
+    ingrediants:{
+      meat:0,
+      bacon:0,
+      salad:0,
+      cheese:0
+    },
+    price :{
+      meat:2.1,
+      bacon:3,
+      salad:1,
+      cheese:1.5
+    },
+    priceTotal:4
+  }
+  
+  
+  render(){
+    return(
+      <Aux>
+        <Header/>
+        <BurgerSection ingrediants={this.state.ingrediants}/>
+        <ManageSection type={this.state.price} />
+      </Aux>
+    )
+  }
 } 
 
 export default BurgerBuilder;
--- src/Components/BurgerBuilder/BurgerSection/BurgerSection.js
+++ src/Components/BurgerBuilder/BurgerSection/BurgerSection.js
@@ -2,15 +2,21 @@ import React from 'react';
 import Aux from '../../HOC/Aux/Aux'
 import Ingrediants from './Ingrediants/Ingrediants';
 import classes from './BurgerSection.module.css'
+
+
+
 const BurgerSection=( props )=>{
+  const ingrediants=Object.keys(props.ingrediants)
+                      .map((igKey)=>{
+                        return [...Array(props.ingrediants[igKey])].map((_,i)=>{
+                          return <Ingrediants type={igKey} key={igKey+i} />
+                        })
+                      });
   return(
     <Aux>
     <div className={classes.Section}>
       <Ingrediants type="bread-top" />
-      <Ingrediants type="salad" />
-      <Ingrediants type="bacon" />
-      <Ingrediants type="meat" />
-      <Ingrediants type="cheese" />
+      {ingrediants}
       <Ingrediants type="bread-bottom" />
     </div>
     </Aux>
