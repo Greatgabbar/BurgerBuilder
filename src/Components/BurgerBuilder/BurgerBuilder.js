b97087f94bf452db5b573b31cf30c16e06569a1b
Shubham Trivedi <strivedi_be19@thapar.edu>
2020-09-10 18:19:20 +0530

    adding loader at main site

--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -9,125 +9,139 @@ import Spinner from './Spinners/Spinner';
 import ErrHandle from '../HOC/ErrModal/ErrModal';
 
 
-const price ={
-  meat:2.1,
-  bacon:3,
-  salad:1,
-  cheese:1.5
+const price = {
+  meat: 2.1,
+  bacon: 3,
+  salad: 1,
+  cheese: 1.5
 }
 
-class BurgerBuilder extends Component{
-  state={
-    ingrediants:{
-      meat:0,
-      bacon:0,
-      salad:0,
-      cheese:0
-    },
-    priceTotal:4,
-    purchasing : true,
-    checkout : false,
-    loading : false
+class BurgerBuilder extends Component {
+  state = {
+    ingrediants: null,
+    priceTotal: 4,
+    purchasing: true,
+    checkout: false,
+    loading: false,
+    error : null
   }
 
-  
-  incrementPrice = (type)=>{
-    const prevPrice=price[type];
-    let prevQuan=this.state.ingrediants[type];
-    let UpdatedQuan={
+  componentDidMount() {
+    axios.get('/ingrediants.json')
+      .then((data) => {
+        console.log(data)
+        this.setState({ ingrediants: data.data });
+      })
+      .catch((err) => {
+        this.setState({error : true})
+      })
+  }
+
+  incrementPrice = (type) => {
+    const prevPrice = price[type];
+    let prevQuan = this.state.ingrediants[type];
+    let UpdatedQuan = {
       ...this.state.ingrediants
     }
-    let prevPT=this.state.priceTotal;
-    prevPT=prevPT + prevPrice;
-    prevQuan=prevQuan+1;
-    UpdatedQuan[type]=prevQuan;
-    let disabled=this.purchasing(prevPT);
+    let prevPT = this.state.priceTotal;
+    prevPT = prevPT + prevPrice;
+    prevQuan = prevQuan + 1;
+    UpdatedQuan[type] = prevQuan;
+    let disabled = this.purchasing(prevPT);
     this.setState({
-      ingrediants : UpdatedQuan,
-      priceTotal : prevPT,
-      purchasing : disabled
+      ingrediants: UpdatedQuan,
+      priceTotal: prevPT,
+      purchasing: disabled
     });
   }
 
 
-  decrementPrice = (type)=>{
-    const prevPrice=price[type];
-    let prevQuan=this.state.ingrediants[type];
-    let UpdatedQuan={
+  decrementPrice = (type) => {
+    const prevPrice = price[type];
+    let prevQuan = this.state.ingrediants[type];
+    let UpdatedQuan = {
       ...this.state.ingrediants
     }
-    let prevPT=this.state.priceTotal;
-    prevPT=prevPT - prevPrice;
-    prevQuan=prevQuan-1;
-    UpdatedQuan[type]=prevQuan;
-    let disabled=this.purchasing(prevPT);
+    let prevPT = this.state.priceTotal;
+    prevPT = prevPT - prevPrice;
+    prevQuan = prevQuan - 1;
+    UpdatedQuan[type] = prevQuan;
+    let disabled = this.purchasing(prevPT);
     this.setState({
-      ingrediants : UpdatedQuan,
-      priceTotal : prevPT,
-      purchasing : disabled
+      ingrediants: UpdatedQuan,
+      priceTotal: prevPT,
+      purchasing: disabled
     });
   }
 
-  purchasing=(price)=>{
-    if(price<=4){
+  purchasing = (price) => {
+    if (price <= 4) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
-  
-  onConfirm=()=>{
-    this.setState({loading : true})
-    const order={
-      ingrediants : this.state.ingrediants,
-      totalPrice : this.state.priceTotal,
-      contact : {
-        name:"Shubham Trivedi",
-        email : "shubham072001@gmail.com",
-        address:"House no. 272A Gali no. 1 Shastri Colony Faridabad",
+
+  onConfirm = () => {
+    this.setState({ loading: true })
+    const order = {
+      ingrediants: this.state.ingrediants,
+      totalPrice: this.state.priceTotal,
+      contact: {
+        name: "Shubham Trivedi",
+        email: "shubham072001@gmail.com",
+        address: "House no. 272A Gali no. 1 Shastri Colony Faridabad",
       }
     };
-    axios.post('/orders.json',order)
-      .then((data)=>{
-        this.setState({loading:true,checkout:false});
+    axios.post('/orders.json', order)
+      .then((data) => {
+        this.setState({ loading: true, checkout: false });
       })
-      .catch((err)=>{
-        this.setState({loading:true,checkout:false});
+      .catch((err) => {
+        this.setState({ loading: true, checkout: false });
       })
   }
 
-  checkouting=()=>{
-    let lol=this.state.checkout;
-    lol=true;
-    this.setState({checkout : lol});
+  checkouting = () => {
+    let lol = this.state.checkout;
+    lol = true;
+    this.setState({ checkout: lol });
   }
-  rcheckouting=()=>{
-    this.setState({checkout:false});
+  rcheckouting = () => {
+    this.setState({ checkout: false });
   }
 
-  render(){
-    let disabledInfo={
+  render() {
+    let disabledInfo = {
       ...this.state.ingrediants
     }
-    for(let key in disabledInfo){
-      disabledInfo[key]=disabledInfo[key] <=0;
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
     }
-    let orders=<OrderSummary type={this.state.ingrediants} price={this.state.priceTotal} cancel={this.rcheckouting} confirm={this.onConfirm} />
-
-    if(this.state.loading){
-      orders=<Spinner />
+    let orders;
+    if (this.state.loading) {
+      orders = <Spinner />
+    }
+    let burger = (this.state.error ? <p>Ingrediants Not Loaded Try Again :)</p> :<Spinner />);
+    if (this.state.ingrediants) {
+      orders = <OrderSummary type={this.state.ingrediants} price={this.state.priceTotal} cancel={this.rcheckouting} confirm={this.onConfirm} />
+      burger = (
+        <Aux>
+          <BurgerSection ingrediants={this.state.ingrediants} />
+          <ManageSection price={this.state.priceTotal} disable={disabledInfo} type={this.state.ingrediants} less={this.decrementPrice} added={this.incrementPrice} purchasing={this.state.purchasing} click={this.checkouting} />
+        </Aux>
+      );
     }
 
-    return(
+    return (
       <Aux>
         <Modal show={this.state.checkout} click={this.rcheckouting}>
           {orders}
         </Modal>
-        <BurgerSection ingrediants={this.state.ingrediants}/>
-        <ManageSection price={this.state.priceTotal} disable={disabledInfo} type={this.state.ingrediants} less={this.decrementPrice} added={this.incrementPrice} purchasing={this.state.purchasing} click={this.checkouting}/>
+        {burger}
       </Aux>
     )
   }
-} 
+}
 
-export default ErrHandle(BurgerBuilder,axios);
+export default ErrHandle(BurgerBuilder, axios);
