f54c9fdf8febf517a5cfabb4ff30732fb7ec9bcd
Shubham Trivedi <strivedi_be19@thapar.edu>
2020-09-16 19:53:32 +0530

    adding complete order page with st~yling

--- src/Components/Orders/Order/Order.js
+++ src/Components/Orders/Order/Order.js
@@ -2,10 +2,18 @@ import React from 'react'
 import classes from './Order.module.css'
 
 const Order = (props) => {
+  const data = [];
+  for (let i in props.order) {
+    data.push({ key: i, val: props.order[i] })
+  }
+  let order = data.map((data) => {
+    return <span style={{ textTransform: 'capitalize', display: 'inline-block', margin: '0 8px', border: '1px solid #ccc',padding : '5px' }}
+      key={data.key}>{data.key} ({data.val}) </span>
+  })
   return (
     <div className={classes.Order}>
-      <p>Ingrediants : Salad (1) , Meat (2) </p>
-      <p>Price : <strong>$45.5</strong></p>
+      <p>Ingrediants : {order} </p>
+      <p>Price : <strong>${props.price}</strong></p>
     </div>
   )
 }
--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,15 +1,53 @@
 import React, { Component } from 'react'
 import Order from './Order/Order'
+import axios from '../../axios-order'
+import ErrModal from '../HOC/ErrModal/ErrModal'
+import Spinner from '../BurgerBuilder/Spinners/Spinner'
 
-class Orders extends Component{
-  render(){
-    return(
+
+class Orders extends Component {
+
+  state = {
+    orders: [],
+    loading: true
+  }
+
+  componentDidMount() {
+    axios.get('orders.json')
+      .then((res) => {
+        const fOrders = [];
+        for (let key in res.data) {
+          fOrders.push({
+            ...res.data[key],
+            id: key
+          });
+        }
+        this.setState({ orders :fOrders,loading: false })
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ loading: false });
+      })
+  }
+
+  render() {
+    let orders = this.state.orders.map((data)=>{
+      console.log(data);
+     return <Order order={data.ingrediants} price={data.totalPrice} key={data.id} />
+    }
+    ) 
+
+
+    if(this.state.loading){
+      orders=(<Spinner />);
+    }
+
+    return (
       <div>
-        <Order />
-        <Order />
+        {orders}
       </div>
     )
   }
 }
 
-export default Orders;
+export default ErrModal(Orders, axios);
